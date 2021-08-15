0bf1fe44b5331bf66a7e235a171bf09a5466a40f
Therou <frombayern@gmail.com>
2021-08-15 09:53:04 -0700

    add files

--- hw7.py
+++ hw7.py
@@ -1,13 +1,19 @@
-users = {}
+import sys
+shells = []
+shells_numbers = {}
+
 with open('/etc/passwd') as f:
     for line in f:
         if not line.startswith('#'):
             user_info = line.split(':')
-            users[user_info[0]] = user_info[6]
+            shells.append(user_info[6].strip())
+            
+shells.sort()
 
+for shell in shells:
+    shells_numbers[shell] = shells_numbers.get(shell, 0) + 1
 
-for username in sorted(users):
-    shells = '{}'.format(users[username])
-
-shells_list = shells.split()
-print(shells_list)
+sys.stdout = open('output.txt', 'w')
+for k, v in shells_numbers.items():
+    print(k, '-', v, '; ', end = '')
+sys.stdout.close()
