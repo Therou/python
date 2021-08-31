11a7956edf4f5ed3c76119c385c92aa6c6536937
Therou <frombayern@gmail.com>
2021-08-31 10:00:55 -0700

    Update

--- hw9.py
+++ hw9.py
@@ -5,6 +5,4 @@ print([a * b * (1000 - a - b) for a in range(1, 1000) for b in range(a, 1000) if
 print((sum(range(1, 101)) * sum(range(1, 101))) - sum(i * i for i in range(1, 101)))
 
 #problem 48
-print(' '.join(str([(0 + i**i) for i in range(1, 1001)]))[-10:])
-#print([(0 + pow(x,x)) % pow(10, 10) for x in range(1, 1001)])
-#print([a for a in range(3, 1000) for b in range(a + 1, 999) if (1000 - a - b) > 0 if ((1000 - a - b) * (1000 - a - b)) == (a * a + b * b)])
+print(sum(pow(n, n) for n in range(1, 1001)) % 10 ** 10)
